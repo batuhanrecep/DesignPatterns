a9410f3d5df886034824d2c537a81af16ed117fa
byrchard <batuhanrecep06@hotmail.com>
2024-07-28 01:22:14 +0300

    .

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -12,6 +12,11 @@ namespace Proxy
         {
             //The Proxy design pattern provides a surrogate or placeholder for another object, controlling access to it and adding additional behavior,
             //such as lazy initialization, access control, or logging, without modifying the original object's code.
+
+
+
         }
     }
+
+
 }
