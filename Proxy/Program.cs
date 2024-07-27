acb1bc8a19dbda0de73388e0887aabe3d0f241a4
byrchard <batuhanrecep06@hotmail.com>
2024-07-27 23:47:22 +0300

    definition sentence about proxy design pattern added

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -10,6 +10,8 @@ namespace Proxy
     {
         static void Main(string[] args)
         {
+            //The Proxy design pattern provides a surrogate or placeholder for another object, controlling access to it and adding additional behavior,
+            //such as lazy initialization, access control, or logging, without modifying the original object's code.
         }
     }
 }
