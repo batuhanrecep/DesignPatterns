f11650429f1f6051b66694c26fae5c8c46c324b0
byrchard <batuhanrecep06@hotmail.com>
2024-07-28 23:05:15 +0300

    Memento design pattern showed with simple example

---
+++ Memento/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Memento design pattern captures and externalizes an object's internal state without violating encapsulation,
+            //allowing the object to be restored to this state later, which is useful for implementing undo mechanisms and maintaining the history of changes.
+
+
+            Book book = new Book
+            {
+                Isbn = "12345",
+                Title = "Sefiller",
+                Author = "Victor Hugo"
+            };
+            book.ShowBook();
+            CareTaker history = new CareTaker();
+            history.Memento = book.CreateUndo();
+            book.Isbn = "54321";
+            book.Title = "VICTOR HUGO";
+
+            book.ShowBook();
+            book.RestoreFromUndo(history.Memento);
+            book.ShowBook();
+
+
+            Console.ReadLine();
+        }
+    }
+
+    class Book
+    {
+        private string _title;
+        private string _author;
+        private string _isbn;
+        private DateTime _lastEdited;
+
+        //quick info, before the .net 2.0, properties weren't had auto property option. We were writing them like below
+        public string Title
+        {
+            get { return _title; }
+            set
+            {
+                _title = value;
+                SetLastEdited();
+            }
+        }
+
+        public string Author
+        {
+            get { return _author; }
+            set
+            {
+                _author = value;
+                SetLastEdited();
+            }
+        }
+
+        public string Isbn
+        {
+            get { return _isbn; }
+            set
+            {
+                _isbn = value;
+                SetLastEdited();
+            }
+        }
+
+        private void SetLastEdited()
+        {
+            _lastEdited = DateTime.UtcNow;
+        }
+
+        public Memento CreateUndo()
+        {
+            return new Memento(_isbn, _title, _author, _lastEdited);
+        }
+
+        public void RestoreFromUndo(Memento memento)
+        {
+            _title = memento.Title;
+            _author = memento.Author;
+            _lastEdited = memento.LastEdited;
+            _isbn = memento.Isbn;
+        }
+
+        public void ShowBook()
+        {
+            Console.WriteLine("{0},{1},{2} edited: {3}",Isbn,Title,Author,_lastEdited);
+        }
+    }
+
+    class Memento
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Isbn { get; set; }
+        public DateTime LastEdited { get; set; }
+
+        public Memento(string isbn, string title, string author, DateTime lastEdited)
+        {
+            Isbn = isbn;
+            Title = title;
+            Author = author;
+            LastEdited = lastEdited;
+        }
+    }
+
+    class CareTaker
+    {
+        public Memento Memento { get; set; }
+    }
+}
