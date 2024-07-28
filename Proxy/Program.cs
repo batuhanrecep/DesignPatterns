c144488246438321446f3899b4a80b67ea79845b
byrchard <batuhanrecep06@hotmail.com>
2024-07-28 19:53:22 +0300

    Example added for this design pattern

--- Proxy/Program.cs
+++ Proxy/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Proxy
@@ -13,10 +14,51 @@ namespace Proxy
             //The Proxy design pattern provides a surrogate or placeholder for another object, controlling access to it and adding additional behavior,
             //such as lazy initialization, access control, or logging, without modifying the original object's code.
 
+            //To test without proxy design pattern to see the difference
+            //CreditManager manager= new CreditManager();
+            //Console.WriteLine(manager.Calculate());
+            //Console.WriteLine(manager.Calculate());
 
+            CreditBase manager = new CreditManagerProxy();
+            Console.WriteLine(manager.Calculate());
+            Console.WriteLine(manager.Calculate());
 
+            Console.ReadLine();
         }
     }
 
+    abstract class CreditBase
+    {
+        public abstract int Calculate();
+    }
 
+    class CreditManager:CreditBase
+    {
+        public override int Calculate()
+        {
+            int result = 1;
+            for (int i = 1; i < 5; i++)
+            {
+                result *= i;
+                Thread.Sleep(1000);
+            }
+            return result;
+        }
+    }
+
+    class CreditManagerProxy:CreditBase
+    {
+        private CreditManager _creditManager;
+        private int _cachedValue;
+        public override int Calculate()
+        {
+            if (_creditManager==null)
+            {
+                _creditManager = new CreditManager();
+                _cachedValue = _creditManager.Calculate();
+            }
+
+            return _cachedValue;
+        }
+    }
 }
