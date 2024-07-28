54eabbbe0cc438bb33377d2b33e5e4193d20a875
byrchard <batuhanrecep06@hotmail.com>
2024-07-28 20:31:05 +0300

    Simple example added for strategy design pattern

--- Strategy/Program.cs
+++ Strategy/Program.cs
@@ -10,8 +10,15 @@ namespace Strategy
     {
         static void Main(string[] args)
         {
-            //
+            //The Strategy design pattern defines a family of algorithms, encapsulates each one, and makes them interchangeable,
+            //allowing the algorithm to vary independently of clients that use it, thus enabling flexible and reusable code.
 
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.CreditCalculatorBase = new After2010CreditCalculator();
+            customerManager.SaveCredit();
+
+            customerManager.CreditCalculatorBase = new Before2010CreditCalculator();
+            customerManager.SaveCredit();
 
 
 
@@ -20,5 +27,35 @@ namespace Strategy
         }
     }
 
+    abstract class CreditCalculatorBase
+    {
+        public abstract void Calculate();
+    }
 
+    class After2010CreditCalculator:CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit calculated using before2010");
+        }
+    }
+
+    class Before2010CreditCalculator : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Credit calculated using after2010");
+        }
+    }
+
+    class CustomerManager
+    {
+        //Normally, we should use injection instead of property
+        public CreditCalculatorBase CreditCalculatorBase { get; set; }
+        public void SaveCredit()
+        {
+            Console.WriteLine("Customer Manager Business");
+            CreditCalculatorBase.Calculate();
+        }
+    }
 }
