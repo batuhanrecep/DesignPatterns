60690f3b13fa9b0d7a9ec366ad886475ea0f79a3
byrchard <batuhanrecep06@hotmail.com>
2024-07-28 23:49:15 +0300

    Example of State Design Pattern added

---
+++ State/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace State
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //the State design pattern allows an object to alter its behavior when its internal state changes,
+            //by encapsulating state-specific behavior within distinct state objects, making the object appear to change its class and promoting cleaner and more maintainable code.
+
+            Context context = new Context();
+            ModifiedState modified = new ModifiedState();
+            modified.DoAction(context);
+            DeletedState deleted = new DeletedState();
+            deleted.DoAction(context);
+            
+            Console.WriteLine(context.GetState().ToString());
+            Console.ReadLine();
+        }
+    }
+
+    interface IState
+    {
+        void DoAction(Context context);
+
+    }
+
+    class ModifiedState:IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State : Modified");
+            context.SetState(this);
+        }
+
+        public override string ToString()
+        {
+            return "Modified";
+        }
+    }
+    class DeletedState : IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State : Deleted");
+            context.SetState(this);
+        }
+        public override string ToString()
+        {
+            return "Deleted";
+        }
+    }
+
+    class AddedState : IState
+    {
+        public void DoAction(Context context)
+        {
+            Console.WriteLine("State : Added");
+            context.SetState(this);
+        }
+
+        public override string ToString()
+        {
+            return "Added";
+        }
+    }
+
+    class Context
+    {
+        private IState _state;
+
+        public void SetState(IState state)
+        {
+            _state = state;
+        }
+
+        public IState GetState()
+        {
+            return _state;
+        }
+
+    }
+}
